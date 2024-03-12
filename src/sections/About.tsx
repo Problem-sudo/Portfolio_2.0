8f624b778b9e49db10a8a23bad1c09e0cb3ba4d1
problem-sudo <anitmaurya563@gmail.com>
2024-03-12 19:43:43 +0530

    File upload

---
+++ src/sections/About.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useRef } from "react";
+import { useInView, motion } from "framer-motion";
+
+function About() {
+  const ref = useRef(null);
+  const isInView = useInView(ref);
+  useEffect(() => {
+    console.log("Element is in view: ", isInView);
+  }, [isInView]);
+  return (
+    <motion.div
+      className="about"
+      id="about"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+      transition={{ duration: 0.6 }}
+      variants={{
+        visible: { opacity: 1, y: -50 },
+        hidden: { opacity: 0, y: 0 },
+      }}
+    >
+      <div className="title">
+        <h2>About Me</h2>
+      </div>
+      <div className="about-grid">
+        <div className="about-grid-info">
+          <p className="about-grid-info-text">
+          
+          I am Anit Maurya, a versatile and driven student pursuing a B.Tech in Electronics and Communication Engineering at the IIIT Nagpur. My journey in technology has been marked by a diverse array of experiences, from delving into front-end development to mastering the intricacies of full-stack web development. </p>
+          <p className="about-grid-info-text">
+          I excel in both front-end and back-end development. Skilled in HTML, CSS, React.js, and Redux Toolkit, I craft dynamic user interfaces. In back-end development, I leverage Node.js and Express.js, alongside MongoDB and MySQL for database management.
+            
+          </p>
+
+          <p className="about-grid-info-text">
+          Beyond technical skills, I lead sessions on web development and competitive programming, fostering a culture of learning. Driven by innovation, I constantly seek new challenges to grow and make an impact in technology.       
+
+          </p>
+          {/* <p className="about-grid-info-text">
+            I am also working on a new project called&nbsp;
+            <Link
+              className="link"
+              target="_blank"
+              href="https://frontendvita.com"
+            >
+              FrontendVita,
+            </Link>
+            &nbsp;aimed at enhancing frontend development skills.
+          </p> */}
+          {/* <p className="about-grid-info-text">
+            Here are a few technologies I’ve been working with recently:
+          </p>
+          <ul className="about-grid-info-list">
+            <li className="about-grid-info-list-item">React</li>
+            <li className="about-grid-info-list-item">React Native</li>
+            <li className="about-grid-info-list-item">Next.js</li>
+            <li className="about-grid-info-list-item">Typescript</li>
+            <li className="about-grid-info-list-item">Redux Toolkit</li>
+            <li className="about-grid-info-list-item">Node.js</li>
+            <li className="about-grid-info-list-item">Elasticsearch</li>
+            <li className="about-grid-info-list-item">Kibana</li>
+            <li className="about-grid-info-list-item">CSS</li>
+          </ul> */}
+        </div>
+        <div className="about-grid-photo">
+          <div className="overlay"></div>
+          <div className="overlay-border"></div>
+          <div className="about-grid-photo-container">
+            <Image src="/Anit.png" alt="profile" fill />
+          </div>
+        </div>
+      </div>
+    </motion.div>
+  );
+}
+
+export default About;
---
+++ src/sections/Projects.tsx
@@ -0,0 +1,152 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { FiGithub, FiExternalLink } from "react-icons/fi";
+import { motion } from "framer-motion";
+
+function Projects() {
+  const projectsData = [
+    {
+      image: "/project1.png",
+      projectName: "Movix",
+      projectLink: "https://movixmod.netlify.app/",
+      projectDescription:
+        
+        "Introducing Movixmod: an online movie and TV show database fueled by the TMDB movie API. Explore a vast collection of film information, including cast, crew, and plot summaries, all in one convenient platform. Join Movixmod and embark on an immersive cinematic journey today!",
+      projectTech: [
+        "React",
+        "Redux Toolkit",
+        "SCSS",
+        "javascript",
+        "TMDB API",
+      ],
+      projectExternalLinks: {
+        github: "https://github.com/Problem-sudo/movie-max",
+        externalLink: "https://movixmod.netlify.app/",
+      },
+    },
+    {
+      image: "/project2.png",
+      projectName: "Realtime Chat App",
+      projectLink: "https://netlify.com",
+      projectDescription:
+        "This is a chat app made with React and Node.js that sends real-time messages and is highly scalable. It uses advanced JavaScript concepts like debouncing and is optimized for React. It has 300+ stars on GitHub and over 180k views on YouTube.",
+      projectTech: [
+        "React",
+        "Sockets",
+        "Node.js",
+        "Express",
+        "MongoDB",
+        "Styled Components",
+      ],
+      projectExternalLinks: {
+        github: "https://github.com/Problem-sudo/Chat-Application",
+        externalLink: "https://github.com/Problem-sudo/Chat-Application",
+      },
+    },
+    {
+      image: "/project3.png",
+      projectName: "Netflix App",
+      projectLink: "https://netlify.com",
+      projectDescription:
+        "I made a Netflix copy with TMBD Api. It has infinite scrolling and lets you watch movies by genre. You can also add movies to your favorites list.",
+      projectTech: [
+        "React",
+        "Node.js",
+        "Firebase",
+        "MongoDB",
+        "Express",
+        "Redux Toolkit",
+      ],
+      projectExternalLinks: {
+        github: "https://github.com/Problem-sudo/OTT-platform",
+        externalLink: "https://github.com/Problem-sudo/OTT-platform",
+      },
+    },
+  ];
+  return (
+    <div className="projects" id="work">
+      <motion.div
+        className="title"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 0.6 }}
+        variants={{
+          visible: { opacity: 1, y: -50 },
+          hidden: { opacity: 0, y: 0 },
+        }}
+      >
+        <h2>Some Things I’ve Built</h2>
+      </motion.div>
+      <div className="projects-container">
+        {projectsData.map(
+          ({
+            image,
+            projectDescription,
+            projectLink,
+            projectExternalLinks,
+            projectName,
+            projectTech,
+          }) => {
+            return (
+              <motion.div
+                className="project"
+                key={projectName}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ duration: 0.6 }}
+                variants={{
+                  visible: { opacity: 1, y: -50 },
+                  hidden: { opacity: 0, y: 0 },
+                }}
+              >
+                <div className="project-image">
+                  <div className="project-image-overlay"></div>
+                  <div className="project-image-container">
+                    <Image src={image} fill alt={projectName} quality={100} />
+                  </div>
+                </div>
+                <div className="project-info">
+                  <p className="project-info-overline">Featured Project</p>
+                  <h3 className="project-info-title">{projectName}</h3>
+                  <div className="project-info-description">
+                    <p>{projectDescription}</p>
+                  </div>
+                  <ul className="project-info-tech-list">
+                    {projectTech.map((tech) => (
+                      <li className="project-info-tech-list-item" key={tech}>
+                        {tech}
+                      </li>
+                    ))}
+                  </ul>
+                  <ul className="project-info-links">
+                    <li className="project-info-links-item">
+                      <Link
+                        href={projectExternalLinks.github}
+                        className="project-info-links-item-link"
+                      >
+                        <FiGithub />
+                      </Link>
+                    </li>
+                    <li className="project-info-links-item">
+                      <Link
+                        href={projectExternalLinks.externalLink}
+                        className="project-info-links-item-link"
+                      >
+                        <FiExternalLink />
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
+              </motion.div>
+            );
+          }
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Projects;
